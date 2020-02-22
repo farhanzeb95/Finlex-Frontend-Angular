a10248a7a6ea83d9ce3f8fc5e5ad9c5951a0e280
farhanzeb95 <farhanzeb95@gmail.com>
2020-02-22 14:53:55 +0100

    fetching data from typescript

--- src/app/contract-objects/contract-objects.component.ts
+++ src/app/contract-objects/contract-objects.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contract-objects.component.css']
 })
 export class ContractObjectsComponent implements OnInit {
+  customerName: string;
+  customerAddress: string;
+  totalPrice: string;
+  brokerName: string;
+  brokerAddress: string;
+  contractStartDate: string;
+  contractEndDate: string;
 
-  constructor() { }
+  constructor() {
+    this.customerName = "First Customer";
+    this.customerAddress = "First Customer Address";
+    this.totalPrice  = "First Customer Total Price";
+    this.brokerName = "First Broker Name";
+    this.brokerAddress = "First Broker Address";
+    this.contractStartDate = "First Customer Contract Start Date";
+    this.contractEndDate = "First Customer Contract End Date"
+   }
 
   ngOnInit() {
+
   }
 
 }
